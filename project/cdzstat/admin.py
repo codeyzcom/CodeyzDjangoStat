ecb9315a2d82cd330c357b667e5ca336961121d4
Atom <a08i80e@outlook.com>
2020-04-13 22:55:04 +0600

    admin. Edit

--- project/cdzstat/admin.py
+++ project/cdzstat/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Host,
+    ExceptionPath
+)
+
+
+@admin.register(Host)
+class HostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'host')
+
+
+@admin.register(ExceptionPath)
+class ExceptionPathAdmin(admin.ModelAdmin):
+    list_display = ('id', 'host', 'path', 'state')
