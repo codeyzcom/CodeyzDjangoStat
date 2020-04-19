722c71f6ef2e7e003bdb71d69912dcd417a07b2f
Atom <a08i80e@outlook.com>
2020-04-19 17:27:09 +0600

    Migrations. Added ExceptionPath initial data into migrate 0002

--- cdzstat/migrations/0002_load_initial_data.py
+++ cdzstat/migrations/0002_load_initial_data.py
@@ -5,7 +5,7 @@ from django.db import migrations
 from django.core import serializers
 
 
-def load_fixture(apps, scheme_editor):
+def load_fixture_user_agent(apps, scheme_editor):
     current_path = Path().absolute()
     fixture_file = os.path.join(
         current_path,
@@ -20,9 +20,19 @@ def load_fixture(apps, scheme_editor):
             obj.save()
 
 
-def unload_fixture(apps, scheme_editor):
+def unload_fixture_user_agent(apps, scheme_editor):
     UserAgent = apps.get_model('cdzstat', 'UserAgent')
     UserAgent.objects.all().delete()
+
+
+def load_exception_path(apps, scheme_editor):
+    ExceptionPath = apps.get_model('cdzstat', 'ExceptionPath')
+    ExceptionPath.objects.create(except_type='regex', path='^/admin/.*')
+
+
+def unload_exception_path(apps, scheme_editor):
+    ExceptionPath = apps.get_model('cdzstat', 'ExceptionPath')
+    ExceptionPath.objects.all().delete()
 
 
 class Migration(migrations.Migration):
@@ -31,5 +41,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(load_fixture, reverse_code=unload_fixture),
+        migrations.RunPython(
+            load_fixture_user_agent,
+            reverse_code=unload_fixture_user_agent
+        ),
+        migrations.RunPython(
+            load_exception_path,
+            reverse_code=unload_exception_path
+        ),
     ]
