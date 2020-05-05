b443dcd9ab8f3afd9b67bf61308cbb057674c9a5
Atom <atom@codeyz.com>
2020-05-06 00:50:48 +0600

    views. Fix error

--- cdzstat/views.py
+++ cdzstat/views.py
@@ -1,17 +1,11 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from cdzstat.services import (
-    HeightLevelService,
-    StatisticalService,
-)
+from cdzstat.services import HeightLevelService
 
 
 @csrf_exempt
 def collector(request):
-    if request.method == 'GET':
-        hls = HeightLevelService(request)
-        hls.process()
     if request.method == 'POST':
         stat_srv = HeightLevelService(request)
         stat_srv.process()
