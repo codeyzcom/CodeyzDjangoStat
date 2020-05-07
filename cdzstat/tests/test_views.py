66bd9c6d56d78804c808fbf1d56ec8fa837aca08
Atom <atom@codeyz.com>
2020-05-07 18:01:03 +0600

    Fixed some tests and added setup.cfg

--- cdzstat/tests/test_views.py
+++ cdzstat/tests/test_views.py
@@ -11,17 +11,10 @@ from cdzstat import views
 class CollectorTest(TestCase):
 
     def setUp(self) -> None:
-        self.req_get = RequestFactory().get('/')
         self.req_post = RequestFactory().post('/')
 
     @mock.patch('cdzstat.views.HeightLevelService')
-    def test_get(self, p_height_lvl_srv):
-        result = views.collector(self.req_get)
-        p_height_lvl_srv.assert_called_once()
-        self.assertEqual(result.status_code, 204)
-
-    @mock.patch('cdzstat.views.StatisticalService')
-    def test_post(self, p_stat_srv):
+    def test_post(self, p_height_lvl_srv):
         result = views.collector(self.req_post)
-        p_stat_srv.assert_called_once()
+        p_height_lvl_srv.assert_called_once()
         self.assertEqual(result.status_code, 204)
