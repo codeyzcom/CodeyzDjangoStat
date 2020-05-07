59f3856e44e463d8f513c7add9fbbe80a114d200
Atom <atom@codeyz.com>
2020-05-07 15:46:26 +0600

    services. Minor refactor

--- cdzstat/services.py
+++ cdzstat/services.py
@@ -45,59 +45,6 @@ class ServiceUtils:
         return result
 
 
-class ExceptionService:
-
-    def __init__(self, request):
-        self._req = request
-
-    def check(self):
-        host = self._req.META.get('HTTP_HOST')
-        path = self._req.path
-        user_agent = self._req.META['HTTP_USER_AGENT']
-
-        if path == '/cdzstat/collect_statistic':
-            return True
-
-        if not CDZSTAT_IGNORE_BOTS and not USER_AGENT_CACHE:
-            USER_AGENT_CACHE.extend(
-                models.UserAgent.objects.filter(
-                    is_bot=True
-                ).order_by('data').values_list('data', flat=True)
-            )
-
-        if not CDZSTAT_IGNORE_BOTS and user_agent in USER_AGENT_CACHE:
-            return True
-
-        if not EXCEPTION_CACHE_REGEX:
-            result = models.ExceptionPath.objects.filter(
-                state=True,
-                except_type='regex',
-                host__host=host
-            ).values_list('path', flat=True)
-            EXCEPTION_CACHE_REGEX[host] = tuple(result)
-
-        regex_path = EXCEPTION_CACHE_REGEX.get(host)
-        if regex_path:
-            for r in regex_path:
-                match = re.match(r, path, re.IGNORECASE)
-                if match:
-                    return True
-
-        if not EXCEPTION_CACHE_DIRECT:
-            result = (
-                models.ExceptionPath.objects.filter(
-                    state=True,
-                    except_type='match',
-                    host__host=host
-                ).values_list('path', flat=True)
-            )
-            EXCEPTION_CACHE_DIRECT[host] = tuple(result)
-
-        direct_paths = EXCEPTION_CACHE_DIRECT.get(host)
-        if direct_paths and path in direct_paths:
-            return True
-
-
 class StoreService:
 
     def __init__(self):
@@ -181,6 +128,59 @@ class StoreService:
             'edge': edge_data,
             'adjacency': adjacency_data
         })
+
+
+class ExceptionService:
+
+    def __init__(self, request):
+        self._req = request
+
+    def check(self):
+        host = self._req.META.get('HTTP_HOST')
+        path = self._req.path
+        user_agent = self._req.META['HTTP_USER_AGENT']
+
+        if path == '/cdzstat/collect_statistic':
+            return True
+
+        if not CDZSTAT_IGNORE_BOTS and not USER_AGENT_CACHE:
+            USER_AGENT_CACHE.extend(
+                models.UserAgent.objects.filter(
+                    is_bot=True
+                ).order_by('data').values_list('data', flat=True)
+            )
+
+        if not CDZSTAT_IGNORE_BOTS and user_agent in USER_AGENT_CACHE:
+            return True
+
+        if not EXCEPTION_CACHE_REGEX:
+            result = models.ExceptionPath.objects.filter(
+                state=True,
+                except_type='regex',
+                host__host=host
+            ).values_list('path', flat=True)
+            EXCEPTION_CACHE_REGEX[host] = tuple(result)
+
+        regex_path = EXCEPTION_CACHE_REGEX.get(host)
+        if regex_path:
+            for r in regex_path:
+                match = re.match(r, path, re.IGNORECASE)
+                if match:
+                    return True
+
+        if not EXCEPTION_CACHE_DIRECT:
+            result = (
+                models.ExceptionPath.objects.filter(
+                    state=True,
+                    except_type='match',
+                    host__host=host
+                ).values_list('path', flat=True)
+            )
+            EXCEPTION_CACHE_DIRECT[host] = tuple(result)
+
+        direct_paths = EXCEPTION_CACHE_DIRECT.get(host)
+        if direct_paths and path in direct_paths:
+            return True
 
 
 class LowLevelService:
