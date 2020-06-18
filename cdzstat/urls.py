59df00f6bf54e899a0d1ce6583d785364b89f8d3
Yevgeniy Zhel <21162@wisk.kz>
2020-06-18 15:05:52 +0600

    Added base admin template and permission view

--- cdzstat/urls.py
+++ cdzstat/urls.py
@@ -4,5 +4,14 @@ from cdzstat import views
 
 app_name = 'cdzstat'
 urlpatterns = [
-    path('cdzstat/collect_statistic', views.collector, name='col_stat')
+    path(
+        'cdzstat/collect_statistic',
+        views.collector,
+        name='col_stat'
+    ),
+    path(
+        'cdzstat/',
+        views.IndexAdminPage.as_view(),
+        name='admin_index',
+    ),
 ]
--- cdzstat/views.py
+++ cdzstat/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View, TemplateView
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 from cdzstat.services import HeightLevelService
 
@@ -10,3 +12,17 @@ def collector(request):
         stat_srv = HeightLevelService(request)
         stat_srv.process()
     return HttpResponse(status=204)
+
+
+class AdminSuperuserMixin(UserPassesTestMixin, View):
+
+    def test_func(self):
+        return self.request.user.is_superuser and self.request.user.is_staff
+
+
+class IndexAdminPage(TemplateView, AdminSuperuserMixin):
+    template_name = 'admin/base_cdzstat_admin.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        return ctx
